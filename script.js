1abdb9c59adb80ea4c1b040905e3c5fa1364f8f5
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-02-02 18:37:11 +0500

    added elements in top div

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let table = document.querySelector("#table");
 let left = document.querySelector("#left");
+let topDiv = document.querySelector("#top")
 
 // CHESS BOARD UI USING LOOPS, DOM
 
@@ -20,15 +21,25 @@ for(let i = 1 ; i <= 8 ; i++){
 const rows = [1, 2, 3, 4, 5, 6, 7, 8];
 const column = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
+// LEFT DIV ELEMENTS
 rows.forEach(leftElement => {
     let leftDiv = document.createElement("div");
     leftDiv.textContent = leftElement;
     left.appendChild(leftDiv);
 });
 
-
+// RIGHT DIV ELEMENTS
 rows.forEach(rightElement => {
     let rightDiv = document.createElement("div");
     rightDiv.textContent = rightElement;
     right.appendChild(rightDiv);
 })
+
+// TOP DIV ELEMENTS
+column.forEach(topElement => {
+    let topDivChild = document.createElement("div");
+    topDivChild.textContent = topElement;
+    topDiv.appendChild(topDivChild);
+})
+
+
